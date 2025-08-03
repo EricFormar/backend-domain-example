bf5b2efb51d16f6913ece188a87a0ec5f61f0353
Eric <menaericdaniel@gmail.com>
2025-08-03 06:50:20 -0300

    feat(user): add user login use case with authentication
    Implement user login functionality with email/password validation and JWT token generation
    Include test cases for invalid credentials and successful login

---
+++ domain/src/use-cases/user/user-login.test.ts
@@ -0,0 +1,62 @@
+import { beforeAll, describe, expect, test } from "vitest";
+import { createUserRepositoryMock, MockedUserRepository } from "../../mocks/user-repository-mock";
+import { createUserMock } from "../../mocks/user-mock";
+import { createUnauthorizedError } from "../../errors/error";
+import { createCryptoServiceMock } from "../../mocks/crypto-repository-mock";
+import { login, UserLoginDependencies, UserLoginResponseModel } from "./user-login";
+
+describe("Login & auth user", () => {
+     const _mockedUserRepository: MockedUserRepository = createUserRepositoryMock([
+        createUserMock({
+            id: "stored-id",
+            email: "stored@email.com",
+            password: "[HASHED]stored-password",
+            role : "user"
+        })
+     ]);
+
+    let _dependencies : UserLoginDependencies;
+
+    beforeAll(async () => {
+       _dependencies = {
+        userRepository: _mockedUserRepository,
+        cryptoRepository : createCryptoServiceMock()
+       }
+    });
+
+    test("With a valid email not stored, fails with error 'INVALID_CREDENTIALS'", async () => {
+        await expect(login(
+            _dependencies,
+            {
+                email: "non-existent@email.com",
+                password: "any password",
+            }
+        )).rejects.toThrow(createUnauthorizedError("Invalid credentials"));
+    });
+
+    test("With an existent email and an invalid password, fails with error 'INVALID_CREDENTIALS'", async () => {
+        await expect(login(
+           _dependencies,
+            {
+                email: "stored@email.com",
+                password: "invalid password",
+            }
+        )).rejects.toThrow(createUnauthorizedError("Invalid credentials"));
+    });
+
+    test("With valid data, returns a valid token", async () => {
+        const result = await login(
+           _dependencies,
+            {
+                email: "stored@email.com",
+                password: "stored-password",
+            }
+        );
+        expect(result).toHaveProperty('token');
+        const JWT = await _dependencies.cryptoRepository.generateJWT(_mockedUserRepository.users[0]);
+        expect((result as UserLoginResponseModel).token).toStrictEqual(JWT);
+        expect(_dependencies.cryptoRepository.validateToken((result as UserLoginResponseModel).token)).toStrictEqual(
+            _dependencies.cryptoRepository.validateToken(JWT)
+        );
+    });
+});
