e181f78a9aea99467b1bfc8697c04e8bd2ad237a
@emena <menaericdaniel@gmail.com>
2025-08-04 12:21:32 -0300

    refactor(user-repository): change return types to Partial<User>
    The return types of all methods in UserRepository interface are now Partial<User> instead of User to allow for more flexible data handling and partial updates

--- domain/src/repositories/user-repository.ts
+++ domain/src/repositories/user-repository.ts
@@ -1,9 +1,9 @@
 import { User } from "../entities/User";
 
 export interface UserRepository {
-    findByEmail(email: string): Promise<User | null>;
-    findById(id: string): Promise<User | null>;
-    create(user: User): Promise<User>;
-    update(user: User): Promise<User>;
-    delete(id: string): Promise<boolean>
+    findByEmail(email: string): Promise<Partial<User> | null>;
+    findById(id: string): Promise<Partial<User> | null>;
+    create(user: User): Promise<Partial<User>>;
+    update(user: User): Promise<Partial<User>>;
+    delete(id: string): Promise<boolean>;
 }
