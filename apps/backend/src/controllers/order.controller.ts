f3883f271e13a9fcd69b4cb9503d9bab58f35180
@emena <menaericdaniel@gmail.com>
2025-08-11 13:37:53 -0300

    feat(order): add create purchase order and update add product functionality
    - Implement createNewPurchaseOrder endpoint to handle order creation
    - Update addProductToPurchaseOrder to validate order existence before adding products
    - Improve error handling and response formatting for both endpoints

--- apps/backend/src/controllers/order.controller.ts
+++ apps/backend/src/controllers/order.controller.ts
@@ -1,29 +1,77 @@
-import { AppError, createInternalServerError } from "@domain";
+import { AppError, createInternalServerError, NotFoundError } from "@domain/errors/error";
 import { addProductToPurchaseItem } from "@domain/use-cases/purchase/add-product-item-purchase";
 import { Request, Response } from "express";
 import { purchaseOrderService } from "@backend/src/services/order.service";
 import { productService } from "src/services/product.service";
-
+import { purchaseOrderCreate } from "@domain/use-cases/purchase/create-purchase-order";
+import { findPurchaseOrderById } from "@domain/use-cases/purchase/find-purchase-order-by-id";
+import { userService } from "../services/user.service";
 export function purchaseOrderController() {
   return {
-    addProductToPurchase: async (req: Request, res: Response) => {
+    createNewPurchaseOrder: async (req: Request, res: Response) => {
       try {
-        const {order,product} = req.body;
-         return await addProductToPurchaseItem({
-                purchaseOrderRepository: purchaseOrderService(),
-                productRepository: productService()
-            }, {
-                order,
-                product
-            }) 
+        const { total, date, status, buyer } = req.body;
+        const newOrder =  await purchaseOrderCreate({
+          purchaseOrderRepository: purchaseOrderService(),
+          userRepository : userService()
+        }, {
+          total,
+          date,
+          status,
+          buyer
+        });
+        return res.status(200).json({
+          ok: true,
+          meta: {
+            url: `${req.protocol}://${req.get('host')}/api/purchase-orders/${('id' in newOrder) ? newOrder.id : ''}`
+          },
+          data: newOrder,
+          message: "Orden de compra creada con éxito",
+        });
+      } catch (e) {        
+        const error =
+          e instanceof AppError
+            ? e
+            : createInternalServerError(
+              "Ups, hubo un error al crear un pedido"
+            );
+        return res.status(error.httpStatus).json({
+          ok: false,
+          message: error.message,
+        });
+      }
+    },
+    // Add product to purchase
+    addProductToPurchaseOrder: async (req: Request, res: Response) => {
+      try {
+        const { product } = req.body;
+        const { id } = req.params;
+        const order = await findPurchaseOrderById({
+          purchaseOrderRepository: purchaseOrderService()
+        }, {
+          id
+        })
+        if (order instanceof NotFoundError) {
+          return res.status(404).json({
+            ok: false,
+            message: "Pedido no encontrado"
+          })
+        }
+        return await addProductToPurchaseItem({
+          purchaseOrderRepository: purchaseOrderService(),
+          productRepository: productService()
+        }, {
+          order,
+          product
+        })
 
       } catch (e) {
         const error =
           e instanceof AppError
             ? e
             : createInternalServerError(
-                "Ups, hubo un error al agregar un producto al pedido"
-              );
+              "Ups, hubo un error al agregar un producto al pedido"
+            );
         return res.status(error.httpStatus).json({
           ok: false,
           message: error.message,
@@ -32,3 +80,4 @@ export function purchaseOrderController() {
     },
   };
 }
+
