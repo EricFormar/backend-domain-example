88a470468e5c5ded089a6229e02be8e9744e0f3e
Eric <menaericdaniel@gmail.com>
2025-08-16 22:47:01 -0300

    feat(purchase-orders): add remove item endpoint and update route params
    Add new delete endpoint for removing items from purchase orders and update route parameter names for better clarity

--- apps/backend/src/routes/purchase-orders.routes.ts
+++ apps/backend/src/routes/purchase-orders.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import { purchaseOrderController } from 'src/controllers/order.controller';
 const router = Router();
-const {createNewPurchaseOrder, addProductToPurchaseOrder, findOrderById} = purchaseOrderController();
+const {createNewPurchaseOrder, addProductToPurchaseOrder, findOrderById, removeItemToPurchaseOrder} = purchaseOrderController();
 router
     .post('/', createNewPurchaseOrder)
-    .get('/:id', findOrderById)
-    .post('/:id/add-product', addProductToPurchaseOrder)
+    .get('/:idOrder', findOrderById)
+    .post('/:idOrder/products/:idProduct', addProductToPurchaseOrder)
+    .delete('/:idOrder/items/:idItem', removeItemToPurchaseOrder)
 export default router
