0f2a420a9fd73b3b62792702b62ee8e045f41723
@emena <menaericdaniel@gmail.com>
2025-08-18 13:44:52 -0300

    refactor(App): clean up code structure and improve readability

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,34 +1,38 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { listProducts } from '@application/use-cases/product/list-products';
-import { HttpProductRepository } from '@infrastructure/repositories/http-product-repository';
-import { Product } from '@domain';
-import { ProductCard } from './components';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { listProducts } from "@application/use-cases/product/list-products";
+import { HttpProductRepository } from "@infrastructure/repositories/http-product-repository";
+import { Product } from "@domain";
+import { ProductCard } from "./components";
 
 function App() {
-  
   const [products, setProducts] = useState<Product[]>([]);
 
-useEffect(() => {
-  const repository = new HttpProductRepository
-  const fetchProducts = async () => {
-    const result = await listProducts(repository);
-    console.log(result);
-    
-    setProducts(result)
-  }
-  fetchProducts()
-}, [])
+  useEffect(() => {
+    const repository = new HttpProductRepository();
+    const fetchProducts = async () => {
+      const result = await listProducts(repository);
+      setProducts(result);
+    };
+    fetchProducts();
+  }, []);
 
   return (
-    <>
-    <img src="./src/assets/react.svg" alt="" />
-    {products.map((product) => (
-      <ProductCard key={product.id} product={product}></ProductCard>
-    ))}
-    </>
-  )
+    <div className="container products-wrapper">
+      <div className="row">
+        <div className="col-12">
+          <h2 className="products-title">Todos los productos</h2>
+        </div>
+        <div className="container products-wrapper">
+          <div className="row">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product}></ProductCard>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
-
+export default App;
